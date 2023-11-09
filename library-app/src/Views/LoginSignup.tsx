e57db6cca69991324bef09c95e9946724c90b944
cuong <cuongcao911@gmail.com>
2023-11-09 14:48:25 +0700

    Fix: UI-LoginSignup page, change for the better UX

--- library-app/src/Views/LoginSignup.tsx
+++ library-app/src/Views/LoginSignup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from 'react'
 import './LoginSignup.css'
 
 const user_icon = require('../images/login-Signup-React-Assets/Assets/person.png');
@@ -6,17 +6,27 @@ const email_icon = require('../images/login-Signup-React-Assets/Assets/email.png
 const password_icon = require('../images/login-Signup-React-Assets/Assets/password.png');
 
 const LoginSignup = () => {
+
+  const [feature, setFeature] = useState('Sign Up');
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{feature}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs-field">
-        <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" placeholder='Name' />
-        </div>
+
+        {/* Show Input Name if the feature is Sign Up, none if the feature is Log In */}
+
+        {feature==='Sign Up'
+          ?<div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder='Name' />
+          </div>
+          :<div className=""></div>
+        }
+        
         <div className="input">
           <img src={email_icon} alt="" />
           <input type="text" placeholder='Email' />
@@ -26,11 +36,20 @@ const LoginSignup = () => {
           <input type="text" placeholder='Password' />
         </div>
       </div>
-      <div className="forgot-pass">Forgot password? <span><a href="">Click here</a></span></div>
+      {/* Show Forgot Password if the feature is Log In */}
+      {feature === 'Sign Up'
+        ? <div></div>
+        :<div className="forgot-pass">Forgot password? <span><a href="">Click here</a></span></div>
+      }
+      
       <div className="submit-container">
         
-        <button className='submit'>Sign Up</button>
-        <button className='submit'>Log In</button>
+        <button className={feature==='Sign Up'?'submit':'submit gray'} onClick={()=>{setFeature('Sign Up')}}>
+          Sign Up
+        </button>
+        <button className={feature==='Log In'?'submit':'submit gray'} onClick={()=>setFeature('Log In')}>
+          Log In
+        </button>
         
       </div>
     
