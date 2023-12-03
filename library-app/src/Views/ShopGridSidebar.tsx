0f30c6a0b8fa7c97fb5da904b363198c1f43279e
cuong <cuongcao911@gmail.com>
2023-12-04 04:02:56 +0700

    Fix: responsiveness, lib app

--- library-app/src/Views/ShopGridSidebar.tsx
+++ library-app/src/Views/ShopGridSidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './ShopGridSidebar.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass,faLocationDot, faPhone, faEnvelope, faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons'
@@ -12,6 +12,9 @@ import Pagination from '../Components/Pagination';
 import { Link } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
+import { Book } from '../Interface/interface';
+import axios from 'axios';
+
 
 // interface Props{
 //     handleChange: any;
@@ -24,6 +27,37 @@ const booklandLogo = require('../images/shop-grid-sidebar-img/logo.png');
 
 const ShopGridSidebar:React.FC = () => {
     const {handleChange,result,handleInputSearch,query} = useShopGridSidebarProps();
+
+    //Search and filter functionality
+    const [filter, setFilter] = useState({
+        author: "",
+        genre: "",
+      });
+      const [books, setBooks] = useState<Book>();
+    
+      const handleSelectedFilter = (type: string, data: string) => {
+        if (type === "author") {
+          setFilter((current) => ({ ...current, author: data }));
+        } else {
+          setFilter((current) => ({ ...current, genre: data }));
+        }
+      };
+    
+      const getBooks = async () => {
+        var searchQuery = `q=${query}&${
+          filter.author && `author=${filter.author}`
+        }&${filter.genre && `genre=${filter.genre}`}`;
+    
+        await axios
+          .get(`http://localhost:8000/books?${searchQuery}`)
+          .then((res) => setBooks(res.data));
+      };
+    
+      useEffect(() => {
+        getBooks();
+      }, [filter, query]);
+
+
     return (
         <div className='page-wrapper'>
             <Header />
